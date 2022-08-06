1c8ace01dba4a55611b4438852aa60e17aa4585d
imakhlaq <imakhlaq@gmail.com>
2022-08-07 00:00:31 +0530

    oops

--- practice2.java
+++ practice2.java
@@ -1,3 +1,23 @@
-public class practice2 {
+class Circle {
+    public int radius;
+
+    public double area() {
+        return Math.PI*this.radius * this.radius;
+    }
+
+}
+class Cylinder extends Circle{
+    public int height;
+    @Override
+    public double area() {
+        return Math.PI*this.radius * this.radius;
+    }
     
+
+}
+
+public class practice2 {
+    public static void main(String[] args) {
+
+    }
 }
